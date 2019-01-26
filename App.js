be713aaabda2f21206b47485038ed957caabc622
Hector Diaz <diazhector98@gmai.com>
2019-01-26 00:38:25 -0600

    Picker added

--- App.js
+++ App.js
@@ -8,23 +8,48 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Picker, FlatList} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
+  state = {
+    location: 'valley',
+    pesticides: [
+      {key: 'Object 1'},
+      {key: 'Object 2'},
+      {key: 'Object 3'}
+    ]
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.locationContainer}>
+          <Text>Choose location</Text>
+          <Picker
+            selectedValue={this.state.location}
+            style={{height: 100, width: 200}}
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({
+                location: itemValue
+              })
+            }}
+          >
+            <Picker.Item label="Valley" value="valley"/>
+            <Picker.Item label="Foothills" value="foothills"/>
+          </Picker>
+          <View>
+            <FlatList 
+              data={this.state.pesticides}
+              renderItem={
+                ({item}) => {
+                return <Text style={styles.pesticideItem}>{item.key}</Text>
+              }
+            }
+            />
+          </View>
+        </View>
       </View>
     );
   }
@@ -33,18 +58,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  locationContainer: {
+    margin: 30,
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  pesticideItem: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
 });
